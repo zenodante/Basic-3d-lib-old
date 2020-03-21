765dd9b86d39f4ac2bcecb7ba3968d3408c3ab0a
zenodante <yeming@outlook.com>
2020-03-21 22:22:22 +0800

    work on 2d functions

--- b3d2d.c
+++ b3d2d.c
@@ -1,14 +1,75 @@
 #include "b3d2d.h"
 
-
-
+#if B3L_ARM  == 1
+__STATIC_FORCEINLINE s32   B3L_RoundingToS(f32 in){
+    s32 result;
+    __ASM ("vcvtr.s32.f32 %0,%1" : "=t"(result) : "t"(in));
+    return result; 
+}
+#else
+__STATIC_FORCEINLINE s32   B3L_RoundingToS(f32 in){
+    return (s32)roundf(in); 
+}
+#endif
 _RAM_FUNC void B3L_DrawTiltmap(fBuff_t *pFBuff,B3L_tiltMap_t *pTiltMap,B3L_texture_t *pUVmap,
-                            u32 screenX, u32 screenY,u32 lineSkip, u32 width, u32 height,u32 tiltMapX,u32 tiltMapY){
+                            u32 screenX, u32 screenY,u32 lineSkip, u32 width, u32 height,s32 tiltMapX,s32 tiltMapY){
+    fBuff_t *pDrawPixel = pFBuff + screenX + screenY*lineSkip; //the first draw pixel position
+    texLUT_t *pLUT = pUVmap->pLUT;
+    u8 transIdx = ((pTiltMap->state)&TILT_TRANSPARENT_INDEX_MASK)>>TILT_TRANSPARENT_INDEX_SHIFT;
+
+    //calculate how many complete tilts in x and y
+    //u32 deltaTiltmapX = 8 - tiltMapX + ((tiltMapX>>3)<<3);
+    //u32 deltaDrawAreaX = width - ((width>>3)<<3);
+    //tiltMapX + deltaTiltmapX
+    u32 xL;
+    u32 xR;
+    u32 yU;
+    u32 yD;
+    u32 column;
+    u32 row;
+}
+//mode7 
+__STATIC_FORCEINLINE void B3L_RastLineOnTiltmap_Repeat(fBuff_t *pFBuff,B3L_tiltMap_t *pTiltMap,B3L_texture_t *pUVmap,
+                                     u32 screenX, u32 screenY, u32 lineSkip, u32 width,f32 tiltMapXS, f32 tiltMapXE, f32 tiltMapYS,f32 tiltMapYE){
     fBuff_t *pDrawPixel = pFBuff + screenX + screenY*lineSkip; //the first draw pixel position
     texLUT_t *pLUT = pUVmap->pLUT;
 
-    //calculate how many complete tilts in x and y
-    u32 deltaTiltmapX = 8 - tiltMapX + ((tiltMapX>>3)<<3);
-    u32 deltaDrawAreaX = width - ((width>>3)<<3);
-    tiltMapX + deltaTiltmapX
+    f32 deltaX = (f32)(tiltMapXE-tiltMapXS);
+    f32 deltaY = (f32)(tiltMapYE-tiltMapYS);
+    f32 tiltX = tiltMapXS;
+    f32 tiltY = tiltMapYS;
+    //f32 tiltMapLength = B3L_Sqrtf(deltaX*deltaX + deltaY*deltaY);
+    f32 stepX = deltaX/((f32)(width-1));
+    f32 stepY = deltaY/((f32)(width-1));
+    fBuff_t color;
+    s32 intTiltX,intTiltY;
+    u32 state  = pTiltMap->state;
+    //u32 xRepeat = B3L_TEST(state,TILT_REPEAT_AT_X_DIRECTION);
+    //u32 yRepeat = B3L_TEST(state,TILT_REPEAT_AT_Y_DIRECTION);
+    f32 tiltMapXPixel = (f32)(pTiltMap->tiltsX*8);
+    f32 tiltMapYPixel = (f32)(pTiltMap->tiltsY*8);
+    u32 tiltMapX = pTiltMap->tiltsX;
+    u8 *pTiltMapData = pTiltMap->data;
+    u32 tiltIndex;
+    u8 *uvData = pUVmap->pData;
+    u8 colorIdx;
+    texLUT_t *pLUT = pUVmap->pLUT;
+    while(width --){  
+        if (tiltX<0){tiltX += tiltMapXPixel;}
+        if (tiltX>=tiltMapXPixel){tiltX -= tiltMapXPixel;}
+        if (tiltY<0){tiltY += tiltMapYPixel;}
+        if (tiltY>=tiltMapYPixel){tiltY -= tiltMapYPixel;}
+        intTiltX = B3L_RoundingToS(tiltX);
+        intTiltY = B3L_RoundingToS(tiltY);
+        //get the XY position color value
+        tiltIndex = pTiltMapData[(intTiltX>>3)+(intTiltY>>3)*tiltMapX];
+        //cal the uv map data offset
+        colorIdx = uvData[((tiltIndex>>4)*128*8)+(tiltIndex&0x0f)*8+(intTiltX&0x07)+(intTiltY&0x07)*128];
+        color = pLUT[colorIdx];
+        *pDrawPixel = color;
+        tiltX +=stepX;
+        tiltY +=stepY;
+        pDrawPixel++;
+    }
+
 }
--- b3d2d.h
+++ b3d2d.h
@@ -3,16 +3,36 @@
 
 #include "b3dlib.h"
 
+/*
+B3L_tiltMap_t state
+   31     2423     1615      87
+   ------------------------------------
+31|$$$$$$$$|&&&&&&&&|********|******BA|0
+  ------------------------------------
+ 
+  A Repeat tilt map at x direction
+  B Repeat tilt map at y direction
+  & The outrange default index
+  $ The transparent index
+  
 
-
+*/
+#define TILT_REPEAT_AT_X_DIRECTION               (0)
+#define TILT_REPEAT_AT_Y_DIRECTION               (1)
+#define TILT_OUT_RANGE_INDEX_MASK         0x00FF0000
+#define TILT_OUT_RANGE_INDEX_SHIFT              (16)
+#define TILT_TRANSPARENT_INDEX_MASK       0xFF000000
+#define TILT_TRANSPARENT_INDEX_SHIFT            (24)
 typedef struct{
+    u32    state;
     u16    tiltsX;//multi 8->pixel number
     u16    tiltsY;
     u8*    data;
 }B3L_tiltMap_t;
 
+//tilt coord could out of range
 extern _RAM_FUNC void B3L_DrawTiltmap(fBuff_t *pFBuff,B3L_tiltMap_t *pTiltMap,B3L_texture_t *pUVmap,
-                            u32 screenX, u32 screenY,u32 lineSkip, u32 width, u32 height,u32 tiltMapX,u32 tiltMapY);
+                            u32 screenX, u32 screenY,u32 lineSkip, u32 width, u32 height,s32 tiltMapX,s32 tiltMapY);
 
 //extern void     B3L_DrawHLine(s32 xa, s32 xb, s32 y, fBuff_t color);
 //extern void     B3L_DrawBox(s32 xa, s32 xb, s32 ya, s32 yb, fBuff_t color);
